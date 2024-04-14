f7fc93c538b2f7acd4530207d42e94d971d35be2
ASUS <2438068487@qq.com>
2024-04-14 12:38:44 +0800

    update

--- src/com/Astar/infoClass/Log.java
+++ src/com/Astar/infoClass/Log.java
@@ -9,12 +9,23 @@ public class Log {
         print("INFO", msg, args);
     }
 
+    public static void info(String msg, String color, Object... args) {
+        System.out.print(color);
+        print("INFO", msg, args);
+    }
+
     public static void error(String msg, Object... args) {
+        print("ERROR", msg, args);
+    }
+
+    public static void error(String msg, String color, Object... args) {
+        System.out.print(color);
         print("ERROR", msg, args);
     }
 
     private static void print(String type, String msg, Object... args) {
         String threadName = Thread.currentThread().getName();
+        // 使用ANSI打印带有颜色的文字
         System.out.print("[" + type + "] " +
                 threadName + " " +
                 new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + " " +
